b1848645f68fc2b5eb9a88326fba216f2d52cdd0
zroitman <zroitman@pipeline.com>
2023-04-26 17:27:59 -0400

    Initial setup

---
+++ src/app.py
@@ -0,0 +1,28 @@
+from db import db
+# from db import ### MODELS
+from flask import Flask, request
+import json
+import os
+
+app = Flask(__name__)
+db_filename = "bucket.db"
+
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///%s" % db_filename
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+app.config["SQLALCHEMY_ECHO"] = True
+
+db.init_app(app)
+with app.app_context():
+    db.create_all()
+
+
+# generalized response formats
+def success_response(data, code=200):
+    return json.dumps(data), code
+
+
+def failure_response(message, code=404):
+    return json.dumps({"error": message}), code
+
+
+# routes
