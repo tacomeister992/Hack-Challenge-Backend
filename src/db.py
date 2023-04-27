54fb2cae2f0106422100c62f5b945b63063f9731
tacomeister992 <pam289@cornell.edu>
2023-04-27 19:47:28 -0400

    Added serialize methods and Date model

--- src/db.py
+++ src/db.py
@@ -18,13 +18,25 @@ class Item(db.Model):
     __tablename__ = "items"
     id = db.Column(db.Integer, primary_key=True, autoincrement=True)
     name = db.Column(db.String, nullable=False)
-    # dates = db.relationship() -> have as separate table
-    # photo = db.relationship()
+    dates = db.relationship("Date", cascade="delete")
     notes = db.relationship("Note", cascade="delete")
+    # photo = db.relationship()
     categories = db.relationship("Category", secondary=association_table, back_populates='items')
 
     def __init__(self, **kwargs):
         self.name = kwargs.get('name')
+
+    def serialize(self):
+        """
+        Serialize an instance of Item
+        """
+        return {
+            "id": self.id,
+            "name": self.name,
+            "dates": [d.serialize() for d in self.dates],
+            "notes": [n.serialize() for n in self.notes],
+            "catergories": [c.serialize() for c in self.categories]
+        }
 
 
 class Note(db.Model):
@@ -37,8 +49,18 @@ class Note(db.Model):
     item_id = db.Column(db.Integer, db.ForeignKey("items.id"), nullable=False)
 
     def __init__(self, **kwargs):
-        self.content = kwargs.get('content')
-        self.item_id = kwargs.get('item_id')
+        self.content = kwargs.get("content", "")
+        self.item_id = kwargs.get("item_id")
+
+    def serialize(self):
+        """
+        Serialize an instance of Note
+        """
+        return {
+            "id": self.id,
+            "content": self.content,
+            "item_id": self.item_id
+        }
 
 
 class Category(db.Model):
@@ -52,10 +74,36 @@ class Category(db.Model):
     items = db.relationship("Item", secondary=association_table, back_populates='categories')
 
     def __init__(self, **kwargs):
-        self.name = kwargs.get('name')
-        self.color = kwargs.get('color')
+        self.name = kwargs.get("name", "")
+        self.color = kwargs.get("color", "")
+
+# note sure yet how to handle dates but I made the model in case we need it
+class Date(db.Model):
+    """
+    Model for date
+    """
+
+    __tablename__ = "dates"
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    date = db.Column(db.String, nullable=False)
+    item_id = item_id = db.Column(db.Integer, db.ForeignKey("items.id"), nullable=False)
+
+    def __init__(self, **kwargs):
+        self.date = kwargs.get("date", "")
+        self.item_id = kwargs.get("item_id")
+
+    def serialize(self):
+        """
+        Serialize an instance of Date
+        """
+        return {
+            "id": self.id,
+            "date": self.date,
+            "item_id": self.item_id
+        }
+
 
 # Item
 # User
 # Category
-# dates -> to have multiple dates for an event, maybe a many-to-many type thing
+# dates -> to have multiple dates for an event, maybe a one-to-many type thing
