dd524a28383a019445eae03619e574cecb178e9b
tacomeister992 <pam289@cornell.edu>
2023-04-30 19:33:52 -0400

    added photo model

--- src/db.py
+++ src/db.py
@@ -4,9 +4,24 @@ import datetime
 import hashlib
 import os
 
+import base64
+import boto
+import io
+from io import BytesIO
+from mimetypes import guess_extension, guess_type
+from PIL import Image
+import random
+import re
+import string
+
 import bcrypt
 
 db = SQLAlchemy()
+
+EXTENSIONS = ["png", "gif", "jpg", "jpeg"]
+BASEDIR = os.getcwd()
+S3_BUCKET_NAME = os.environ.get("S3_BUCKET_NAME")
+S3_BASE_URL = f"https://{S3_BUCKET_NAME}.s3.us.east-1.amazonaws.com"
 
 association_table = db.Table(
     "association", db.Model.metadata,
@@ -207,7 +222,76 @@ class Photo(db.Model):
     """
     Model for photo
     """
-    pass
+    __tablename__ = "photos"
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    base_url = db.Column(db.String, nullable=False)
+    salt = db.Column(db.String, nullable=False)
+    extension = db.Column(db.String, nullable=False)
+    width = db.Column(db.Integer, nullable=False)
+    height = db.Column(db.Integer, nullable=False)
+    created_at = db.Column(db.DateTime, nullable=False)
+
+    def __init__(self, **kwargs):
+        self.create(kwargs.get("image_data"))
+
+    def create(self, image_data):
+        """
+        Given an image in base64 form, does the following:
+        1. Rejects the image if its not supported filetype
+        2. Generates a random string for the image filename
+        3. Decodes the image and attempts to upload it to AWS
+        """
+
+        try:
+            ext = guess_extension(guess_type(image_data))[0][1:]
+
+            if ext not in EXTENSIONS:
+                raise Exception(f"Extension {ext} not supported")
+            
+            salt = "".join(
+                random.SystemRandom().choice(
+                string.ascii_uppercase + string.digits
+                )
+                for _ in range(16)
+            )
+
+            img_str = re.sub("^data:image/.+;base64,", "", image_data)
+            img_data = base64.b64decode(img_str)
+            img = Image.open(BytesIO(img_data))
+
+            self.base_url = S3_BASE_URL
+            self.salt = salt
+            self.extension = ext
+            self.width = img.width
+            self.height = img.height
+            self.created_at = datetime.datetime.now()
+
+            img_filename = f"{self.salt}.{self.extension}"
+            self.upload(img, img_filename)
+
+        except Exception as e:
+            print(f"Error while creating image: {e}")
+
+    def upload(self, img, img_filename):
+        """
+        Attempt to upload the image into bucket
+        """
+
+        try:
+            img_temploc = f"{BASEDIR}/{img_filename}"
+            img.save(img_temploc)
+
+            s3_client = boto.client("s3")
+            s3_client.upload_file(img_temploc, S3_BUCKET_NAME, img_filename)
+
+            s3_resource = boto.resource("s3")
+            object_acl = s3_resource.OcjectAc(S3_BUCKET_NAME, img_filename)
+            object_acl.put(ACL="public-read")
+
+            os.remove(img_temploc)
+
+        except Exception as e:
+            print(f"Error while uploading image: {e}")
 
 
 
